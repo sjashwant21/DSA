d1c40f4d319e2ea5380a9e0534fb9e99eaf78981
Shaan Jashwant <109244010+sjashwant21@users.noreply.github.com>
2025-09-06 23:59:26 +0530

    Time: 1853 ms (50.00%) | Memory: 51.2 MB (50.00%) - LeetSync

---
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minOperations(self, queries):
+        """
+        :type queries: List[List[int]]
+        :rtype: int
+        """
+        a=0
+        for q in queries:
+            l,r=q 
+            t=0 
+            ln=1 
+            ln2=1
+            while ln<=r:
+                s=max(l,ln)
+                e=min(r,ln*4-1)
+                if e>=s:
+                    t+= (e-s+1)*ln2
+                ln*= 4
+                ln2+=1
+            a+=(t//2)+(t%2) #total number of steps in this query ceil without ceil
+        return a
