d285b00f215cd1365ae7c25e4ea3e277b5b84c9f
Shaan Jashwant <109244010+sjashwant21@users.noreply.github.com>
2025-09-12 12:37:09 +0530

    Time: 113 ms (22.73%) | Memory: 13.1 MB (43.18%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def doesAliceWin(self, s):
+        vowels = set('aeiou')
+        even = 1  
+        odd = 0
+        parity = 0
+        
+        for ch in s:
+            if ch in vowels:
+                parity ^= 1  
+            if parity == 0:
+                even += 1
+            else:
+                odd += 1
+
+        return even * odd > 0
